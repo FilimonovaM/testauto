ca8733b4d5911d9a2966161a7eee16a0987193e1
Margarita_Filimonova <filimonovamargarita@mail.ru>
2018-01-12 00:15:35 +0300

    lesson 1(selenium) -> home work completed!

--- src/test/java/unit1/hw1/CheckFunctions.java
+++ src/test/java/unit1/hw1/CheckFunctions.java
@@ -9,9 +9,12 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class CheckFunctions {
     WebDriver driver;
 
+    //Open test site by URL
     @BeforeMethod
     public void prepareBrowser() {
         driver = new ChromeDriver();
@@ -19,9 +22,13 @@ public class CheckFunctions {
         driver.navigate().to("https://jdi-framework.github.io/tests/index.htm");
     }
 
+    //Create a new test in a new Java class, specify test name in accordance with checking functionality
     @Test
-    public void checkBrowserTitle() {
+    public void checkPageFunctionality() {
+        //Assert Browser title
         Assert.assertEquals(driver.getTitle(), "Index Page");
+
+        //Perform login
         WebElement logInForm = driver.findElement(By.xpath("/html/body/div/header/div/nav/ul[2]/li"));
         logInForm.click();
         logInForm = driver.findElement(By.id("Login"));
@@ -30,11 +37,48 @@ public class CheckFunctions {
         logInForm.sendKeys("1234");
         logInForm = driver.findElement(By.xpath("/html/body/div/header/div/nav/ul[2]/li/div/form/button"));
         logInForm.click();
-        Assert.assertEquals("", driver.findElement(By.xpath("/html/body/div/header/div/nav/ul[2]/li")));
+        logInForm = driver
+                .findElement(By.xpath("/html/body/div/header/div/nav/ul[2]/li/a/div/span"));
+
+        //Assert User name in the left-top side of screen that user is logged
+        Assert.assertTrue(logInForm.isDisplayed());
+        Assert.assertEquals(logInForm.getText(), "PITER CHAILOVSKII");
+
+        //Assert Browser title
+        Assert.assertEquals(driver.getTitle(), "Index Page");
+
+        //Assert that there are 4 images on the Home Page and they are displayed
+        List<WebElement> images = driver.findElements(By.className("benefit-icon"));
+        Assert.assertEquals(images.size(), 4);
+        images.forEach(list -> Assert.assertTrue(list.isDisplayed()));
+
+        //Assert that there are 4 texts on the Home Page and check them by getting texts
+        List<WebElement> texts = driver.findElements(By.className("benefit-txt"));
+        texts.forEach((list -> Assert.assertTrue(list.isDisplayed())));
+        Assert.assertEquals(texts.size(), 4);
+        String[] messages = {"To include good practices\nand ideas from successful\nEPAM projec",
+                "To be flexible and\ncustomizable",
+                "To be multiplatform",
+                "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…"};
+        for (int i = 0; i < messages.length; i++) {
+            Assert.assertEquals(texts.get(i).getText(), messages[i]);
+        }
+
+        //Assert that there are the main header and the text below it on the Home Page
+        WebElement textElement = driver.findElement(By.cssSelector("h3.main-title.text-center"));
+        Assert.assertEquals(textElement.getText(), "EPAM FRAMEWORK WISHES…");
+        Assert.assertTrue(textElement.isDisplayed());
+        textElement = driver.findElement(By.cssSelector("p.main-txt.text-center"));
+        Assert.assertEquals(textElement.getText(), "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING " +
+                "ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, " +
+                "QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO CONSEQUAT DUIS AUTE " +
+                "IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.");
+        Assert.assertTrue(textElement.isDisplayed());
     }
 
+    //Close Browser
     @AfterMethod
     public void closeResources() {
-//        driver.close();
+        driver.close();
     }
 }
