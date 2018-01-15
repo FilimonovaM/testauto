98970740c3de89964214ed62a33518531cd7873d
Margarita_Filimonova <filimonovamargarita@mail.ru>
2018-01-15 17:08:44 +0300

    hw1 -> edited

--- src/test/java/hw/hw1/CheckLogInTest.java
+++ src/test/java/hw/hw1/CheckLogInTest.java
@@ -11,7 +11,7 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
-public class CheckFunctionsTest {
+public class CheckLogInTest {
     WebDriver driver;
 
     //1 Open test site by URL
