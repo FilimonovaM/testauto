c8afe3ea817b16d93d4b2aa51ccf7a756e1bfeb7
Margarita_Filimonova <filimonovamargarita@mail.ru>
2018-01-15 18:17:02 +0300

    less3 -> added new classes

---
+++ src/test/java/lesson3/SeleniumPageObjectTest.java
@@ -0,0 +1,32 @@
+package lesson3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pageObjects.IndexPage;
+
+public class SeleniumPageObjectTest {
+    private WebDriver driver;
+    private IndexPage indexPage;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUpPage() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        indexPage = PageFactory.initElements(driver, IndexPage.class);
+    }
+    //Close Browser
+    @AfterMethod(alwaysRun = true)
+    public void closeResources() {
+        driver.close();
+    }
+
+    @Test
+    public void simpleTest1() {
+        driver.navigate().to("https://jdi-framework.github.io/tests/index.htm");
+        indexPage.login("epam", "1234");
+    }
+}
