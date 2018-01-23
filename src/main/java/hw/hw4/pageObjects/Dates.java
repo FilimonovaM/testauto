1801a259d6a5a0e04479cda73a03f680aef4a973
Margarita_Filimonova <filimonovamargarita@mail.ru>
2018-01-23 21:10:49 +0300

    hw4 -> done

--- src/main/java/hw/hw4/pageObjects/Dates.java
+++ src/main/java/hw/hw4/pageObjects/Dates.java
@@ -18,7 +18,8 @@ public class Dates {
         actions().dragAndDropBy(nodes.get(1), 1000, 0).build().perform();
         double scrollPanelLength = nodes.get(1).getLocation().getX() - nodes.get(0).getLocation().getX();
         double step = scrollPanelLength / 100;
-        actions().dragAndDropBy(nodes.get(0), (int) (left * step-((left>0)?0.5*step:step)), 0).build().perform();
+        actions().dragAndDropBy(nodes.get(0), (int) (left * step-((left>0)?0.5*step:step)), 0).build()
+                .perform();
         actions().dragAndDropBy(nodes.get(1), (int) (-((100 - right) * step+step)), 0).build().perform();
         Assert.assertEquals(Utile.makeInt(nodes.get(0).getText()), left);
         Assert.assertEquals(Utile.makeInt(nodes.get(1).getText()), right);
